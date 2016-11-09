4304b22cb32cf7d8aa0bc37c148a8f8fb039ac19
JosechuSK8 <josechu_sf@hotmail.com>
2016-11-09 18:19:42 +0100

    Prefab de moneda

---
+++ Assets/Scripts/moneda.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moneda : MonoBehaviour {
+	private Rigidbody2D rb;
+
+
+
+	void start () {
+		Destroy (gameObject,3);
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2 (-100, 100));
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log ("Ha tocado");
+		Destroy (gameObject);
+
+	}
+
+		void OnTriggerExit2D(Collider2D col){
+			Debug.Log ("Han dejado de tocarme");
+		}
+
+		void OnTriggerStay2D(Collider2D col ){
+		Debug.Log ("me estan tocando me siento incomodo");
+		}
+		
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
