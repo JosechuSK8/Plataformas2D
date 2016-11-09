2e9153dfa3d0d6762a5773b29297e2d8ef1be1d2
JosechuSK8 <josechu_sf@hotmail.com>
2016-11-09 16:44:47 +0100

    Subida inicial
    Archivos con el proyecto inicial

---
+++ Assets/Scripts/Player1_Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1_Controller : MonoBehaviour {
+	Animator anim;
+	Rigidbody2D rb_player;
+	public float fuerza = 100f;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> (); // Coger el animator del elemento que tenga el script
+		rb_player = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			anim.SetFloat ("Velocidad",1f); //cuando presiones la tecla A, se activa
+		}
+
+		if (Input.GetKeyUp (KeyCode.A)) {
+			anim.SetFloat ("Velocidad",0f); //cuando presiones la tecla A, se activa
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool ("Jump",true); //cuando presiones la tecla A, se activa
+			GetComponent<Rigidbody2D> ().AddForce (Vector2.up*fuerza);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			anim.SetBool ("Jump",false); //cuando presiones la tecla A, se activa
+		}
+	}
+
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public float velocidad = 5f;
+	public float power = 1f;
+	public float fuerza = 100f;
+	public float Jump = 400f;
+	public bool tocando_suelo = false;
+	private Animator animator;
+	private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
+				}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		animator.SetFloat ("Velocidad", Mathf.Abs (rb.velocity.x));
+
+	
+		if (Input.GetKey ("right")) {
+			rb.velocity = new Vector2 (velocidad*power, rb.velocity.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+		if (Input.GetKey ("left")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+				animator.SetBool ("Jump",true); //cuando presiones la tecla A, se activa
+				rb.AddForce (transform.up*Jump) ;
+				Debug.Log ("Salto");
+			}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+				animator.SetBool ("Jump",false); //cuando presiones la tecla A, se activa
+			}
+			
+
+		}
+		void OnTriggerStay2D(Collider2D objeto) {
+		if (objeto.tag == "Suelo") {
+		}
+
+}
