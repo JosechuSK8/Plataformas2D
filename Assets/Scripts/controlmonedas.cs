5aa56a4a0a542041ba1d08b14b86132274eabec5
JosechuSK8 <josechu_sf@hotmail.com>
2016-11-11 16:53:43 +0100

    Panel moneda y sumar

---
+++ Assets/Scripts/controlmonedas.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class controlmonedas : MonoBehaviour {
+	private int monedas = 0;
+	Text texto;
+
+	void Start(){
+		texto = GetComponent<Text> ();
+
+	}
+
+	public void suma_monedas(int cantidad){
+		monedas = monedas + cantidad; //monedas += cantidad;
+
+		if (monedas < 0) {
+			monedas = 0;
+
+		}
+
+		texto.text = monedas.ToString ();
+
+		Debug.Log ("Monedas: " + monedas);
+
+	}
+}
---
+++ Assets/Scripts/generador.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generador : MonoBehaviour {
+		public GameObject moneda;
+
+	void OnTriggerEnter2D(Collider2D Objeto){
+		if (Objeto.tag == "Player") {
+			Instantiate (moneda, transform.position, transform.rotation);
+
+
+		}
+	}
+}
