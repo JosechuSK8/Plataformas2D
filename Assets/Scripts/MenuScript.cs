d7e967b72512cf41ae246e05d6efec7037ae3e36
JosechuSK8 <josechu_sf@hotmail.com>
2016-11-22 20:30:25 +0100

    Menu conscript

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(Animator))]
+	public class MenuScript : MonoBehaviour {
+	Animator anim;
+
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+
+		void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Pausa ();
+		}
+		}
+
+		public void Pausa (){
+		anim.SetBool ("opciones", false);
+		anim.SetBool ("pausa", true);
+		}
+
+		public void Salir(){
+		Application.Quit ();
+		Debug.Log ("Saliendo");
+
+
+		}
+
+		public void Opciones (){
+		anim.SetBool ("opciones", true);
+		anim.SetBool ("pausa", true);
+		}
+
+	public void continuar (){
+		anim.SetBool ("opciones", false);
+		anim.SetBool ("pausa", false);
+	}
+}
